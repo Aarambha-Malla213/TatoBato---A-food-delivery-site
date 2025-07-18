9f6815b8bd36e95219020a9218894be5916cc16c
Aarambha Bom Malla <mallaaarambha213@gmail.com>
2025-07-18 22:11:43 +0545

    Modification in about us section

--- Frontend/src/components/About Us/AboutUs.jsx
+++ Frontend/src/components/About Us/AboutUs.jsx
@@ -9,10 +9,20 @@ const AboutUs = () => {
         speed, style, and a local touch. Crafted for convenience and designed
         with love, our platform connects hungry customers to trusted restaurants
         across the city making food delivery seamless, satisfying, and just a
-        tap away.  Whether it's a midnight craving or a hearty family dinner,
+        tap away. Whether it's a midnight craving or a hearty family dinner,
         Tatobato is here to deliver happiness, one bite at a time.
       </p>
-      <p className="credit">In efforts of:<br />1. Aarambha Bom Malla <br />2. Pranjal Barnwal</p>
+      <div className="credit">
+        <p>In efforts of:</p>
+        <div className="names">
+          <span onClick={() => window.open("https://www.facebook.com/aarambha.malla", "_blank")}>
+            Aarambha Bom Malla
+          </span>
+          <span onClick={() => window.open("https://www.facebook.com/pranjal.barnwal.39", "_blank")}>
+            Pranjal Barnwal
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
